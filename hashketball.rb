9ab159407705ba768c8fe41446d60b14af7ac177
Brian Lego <legobw@gmail.com>
2020-06-28 04:19:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,20 @@
-# Write your code here!
+
 def game_hash
+  game_hash = {
+    :home => {
+      :team_name => "",
+      :colors => [],
+      :players => [
+        ]
+    },
+    :away => {
+      :team_name => "",
+      :colors => [],
+      :players => [{:player_name => ""}, {:number => ""}, {:shoe => ""}, {:points = },
+        ]
+    }
+  }
+  
+  
   
 end
